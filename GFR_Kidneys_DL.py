a414fdb2994a618414e15bf6e160924bccf49155
User <s1807956@ed.ac.uk>
2022-10-19 13:01:41 +0200

    No change here

--- GFR_Kidneys_DL.py
+++ GFR_Kidneys_DL.py
@@ -22,20 +22,14 @@ from models import *
 from datasets import *
 
 def main():
-    # Open the .yaml file and extract the parameters
     with open("configs/training_config.yaml", "r") as stream:
         config = yaml.safe_load(stream)
-        print(config)
 
-    # Check if the cuda library is available - if yes, run
-    # the pytorch network on GPU
     cuda = torch.cuda.is_available()
-    print("GPU used" if cuda else "CPU used")
+    if cuda: print ("GPU used") 
 
-    # Make a directory where the model for the experiment (epoch)
-    # is saved. Afterwards, create the model using the parameters
-    # from the .yaml file with the input and output dimensions
     os.makedirs("saved_models/%s" % config['experiment_name'], exist_ok = True)
+
     model = NeuralNetwork(input_dim = config['model']['input_dim'], output_dim = config['model']['output_dim'])
     if cuda:
         model.cuda()
@@ -45,33 +39,19 @@ def main():
     if config['training']['epoch'] != 0:
         model.load_state_dict(torch.load("saved_models/%s/%d.pth" % (config['experiment_name'], config['epoch'])))
 
-    # Declare the optimizer used for the neural network, with the 
-    # learning rate specified from the .yaml file. As loss function, the
-    # mean squared error is implemented
     optimiser = optim.SGD(model.parameters(), lr=config['training']['lr'])
     criterion = nn.MSELoss()
 
-    # Implement the dataloader method from pytorch to load the input data as
-    # the .npy files describing the curves and the parameters. Also declare the
-    # batch size and the number of cpus available
-    if config['dataset_name'] == 'synthetic':
+    if config['dataset_name'] == "synthetic":
         dataloader = DataLoader(
-            CustomDataset("data/synthetic/synthetic_curves.npy", "data/synthetic/synthetic_params.npy"),
+            CustomDataset("datasplits/synthetic/train/curves.txt", "datasplits/synthetic/train/params.txt"),
             batch_size=config['training']['batch_size'],
             shuffle=True,
-            num_workers=config['training']['n_cpu'],
-        )
-    prev_time = time.time()
-    
-    # Now start iterating through the epochs - have 200 epochs required for 
-    # training the neural network
-    for epoch in range(config['training']['epoch'], config['training']['n_epochs']):
+            num_workers=config['training']['n_cpu'])
+        
 
-        # For each epoch, have the batch size of 50 as specified in the
-        # .yaml file and the set of 20 batches -> For each batch, have the 
-        # X array of [50, 150] corresponding to the convolution between the
-        # AIF and the VIRF, and the Y array of [50, 4] corresponding to the
-        # parameters for the curve
+    prev_time = time.time()
+    for epoch in range(config['training']['epoch'], config['training']['n_epochs']):
         for i, batch in enumerate(dataloader):
 
             curves = Variable(batch["X"].type(Tensor))
@@ -83,7 +63,7 @@ def main():
             loss.backward()
             optimiser.step()
 
-            # Determine the approximate time left
+            # Determine approximate time left
             batches_done = epoch * len(dataloader) + i
             batches_left = config['training']['n_epochs'] * len(dataloader) - batches_done
             time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
