7f48645b1db9699775cab1f681a9b78e8ca6d886
AlexandruHau <57918379+AlexandruHau@users.noreply.github.com>
2022-10-31 11:57:49 +0000

    Add files via upload
    I changed the last 150 elements of the network output to the non-linear ToftsModel Transformation of the first 4 elements.

--- GFR_Kidneys_DL.py
+++ GFR_Kidneys_DL.py
@@ -20,14 +20,26 @@ from torch.autograd import Variable
 
 from models import *
 from datasets import *
+from ToftsModel import *
 
 def main():
+
+    # Open the AIF and temporal files
+    AIF = np.load("AIF.npy")
+    t = np.load("t.npy")
+
+    # Open the .yaml file and extract the parameters
     with open("configs/training_config.yaml", "r") as stream:
         config = yaml.safe_load(stream)
 
+    # Check if the cuda library is available - if yes, run
+    # the pytorch network on GPU
     cuda = torch.cuda.is_available()
     if cuda: print ("GPU used") 
 
+    # Make a directory where the model for the experiment (epoch)
+    # is saved. Afterwards, create the model using the parameters
+    # from the .yaml file with the input and output dimensions
     os.makedirs("saved_models/%s" % config['experiment_name'], exist_ok = True)
 
     model = NeuralNetwork(input_dim = config['model']['input_dim'], output_dim = config['model']['output_dim'])
@@ -39,9 +51,15 @@ def main():
     if config['training']['epoch'] != 0:
         model.load_state_dict(torch.load("saved_models/%s/%d.pth" % (config['experiment_name'], config['epoch'])))
 
+    # Declare the optimizer used for the neural network, with the 
+    # learning rate specified from the .yaml file. As loss function, the
+    # mean squared error is implemented
     optimiser = optim.SGD(model.parameters(), lr=config['training']['lr'])
     criterion = nn.MSELoss()
 
+    # Implement the dataloader method from pytorch to load the input data as
+    # the .npy files describing the curves and the parameters. Also declare the
+    # batch size and the number of cpus available
     if config['dataset_name'] == "synthetic":
         dataloader = DataLoader(
             CustomDataset("datasplits/synthetic/train/curves.txt", "datasplits/synthetic/train/params.txt"),
@@ -49,17 +67,30 @@ def main():
             shuffle=True,
             num_workers=config['training']['n_cpu'])
         
-
+    # Now start iterating through the epochs - have 200 epochs required for 
+    # training the neural network
     prev_time = time.time()
     for epoch in range(config['training']['epoch'], config['training']['n_epochs']):
+
+        # For each epoch, have the batch size of 50 as specified in the
+        # .yaml file and the set of 20 batches -> For each batch, have the 
+        # X array of [50, 150] corresponding to the convolution between the
+        # AIF and the VIRF, and the Y array of [50, 4] corresponding to the
+        # parameters for the curve
         for i, batch in enumerate(dataloader):
 
             curves = Variable(batch["X"].type(Tensor))
             gt = Variable(batch["Y"].type(Tensor))
-
+            test_tensor = torch.cat([gt, curves], dim = 1)
             model.zero_grad()
             output = model(curves)
-            loss = criterion(output, gt)
+
+            np_output = output.clone().detach().cpu().numpy()
+            for i in range(np_output.shape[0]):
+                np_output[i][4:] = ToftsModel(np_output[i][:4], t, AIF)
+            output[4:] = torch.from_numpy(np_output[4:]).to(output)
+
+            loss = criterion(output, test_tensor)
             loss.backward()
             optimiser.step()
 
