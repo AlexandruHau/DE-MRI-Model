a4d77682549eb168a8e947992dfc48b10d70b6d3
AlexandruHau <57918379+AlexandruHau@users.noreply.github.com>
2022-12-05 09:17:57 +0100

    Add files via upload

--- GFR_Kidneys_DL.py
+++ GFR_Kidneys_DL.py
@@ -21,6 +21,25 @@ from torch.autograd import Variable
 from models import *
 from datasets import *
 
+# Implement the early stopper class in order to avoid
+# overfitting the network
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
 def main():
     with open("configs/training_config.yaml", "r") as stream:
         config = yaml.safe_load(stream)
@@ -43,16 +62,23 @@ def main():
     criterion = nn.MSELoss()
 
     if config['dataset_name'] == "synthetic":
-        dataloader = DataLoader(
+        train_dataloader = DataLoader(
             CustomDataset("datasplits/synthetic/train/curves.txt", "datasplits/synthetic/train/params.txt"),
             batch_size=config['training']['batch_size'],
+            shuffle=True,
+            num_workers=config['training']['n_cpu'])
+
+        validation_dataloader = DataLoader(
+            CustomDataset("datasplits/synthetic/validate/curves.txt", "datasplits/synthetic/validate/params.txt"),
             shuffle=True,
             num_workers=config['training']['n_cpu'])
         
 
     prev_time = time.time()
+    early_stopper = EarlyStopper(patience=5, min_delta=0.5)
+
     for epoch in range(config['training']['epoch'], config['training']['n_epochs']):
-        for i, batch in enumerate(dataloader):
+        for i, batch in enumerate(train_dataloader):
 
             curves = Variable(batch["X"].type(Tensor))
             gt = Variable(batch["Y"].type(Tensor))
@@ -64,15 +90,36 @@ def main():
             optimiser.step()
 
             # Determine approximate time left
-            batches_done = epoch * len(dataloader) + i
-            batches_left = config['training']['n_epochs'] * len(dataloader) - batches_done
+            batches_done = epoch * len(train_dataloader) + i
+            batches_left = config['training']['n_epochs'] * len(train_dataloader) - batches_done
             time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
             prev_time = time.time()
             # Print log
             sys.stdout.write(
                 "\r[Epoch %d/%d] [Batch %d/%d] Loss: %f] ETA: %s"
-                % (epoch, config['training']['n_epochs'], i, len(dataloader), loss.item(), time_left)
+                % (epoch, config['training']['n_epochs'], i, len(train_dataloader), loss.item(), time_left)
             )
+
+        validation_loss = 0
+
+        for i, batch in enumerate(validation_dataloader):
+
+            curves = Variable(batch["X"].type(Tensor))
+            gt = Variable(batch["Y"].type(Tensor))
+
+            model.zero_grad()
+            output = model(curves)
+            loss = criterion(output, gt)
+            loss.backward()
+            optimiser.step()  
+
+            validation_loss += loss.item()
+
+        validation_loss = validation_loss / len(validation_dataloader)          
+        print(f"Validation loss at epoch {epoch} is: {validation_loss}")
+        if early_stopper.early_stop(validation_loss):
+            torch.save(model.state_dict(), "saved_models/%s/%d.pth" % (config['experiment_name'], epoch))             
+            break
 
     torch.save(model.state_dict(), "saved_models/%s/%d.pth" % (config['experiment_name'], epoch))
 
--- ToftsModel.py
+++ ToftsModel.py
@@ -4,40 +4,43 @@ import numpy as np
 from scipy import interpolate
 import scipy.integrate
 import matplotlib.pyplot as plt
+import os
+import sys
 
 # Create the Tofts Model function which takes as input the
 # four parameters with the time and the Arterial Input Function
 # numpy arrays
+
+# Declare a global calibration constant for the K-value - this
+# will be heavily used in the neural network as well
+
+K_calibration = 600
+
 def ToftsModel(params, t, AIF):
 
-    # Assign the parameter names: 
-    Ktrans, Tg, vp, delta = params 
+    # Assign parameter names.  Ktrans is in ml/ml Tissue/second, Tg is in s, vp is between 0 and 1 and Delta is in s
+    Ktrans, Tg, vp, Delta = params
 
-    # Create the vasculat impulse response function taking
-    # the form of an exponential where the argument of the 
-    # exponential is given by the time shift (t - delta)
-    imp = (1 / Tg) * np.exp(-1 * (t - delta) / Tg)
-    imp[np.where(t < delta)] = 0
-    
     #Interpolate the AIF to upsample by a factor of 10 to avoid errors
-    '''
     f = interpolate.interp1d(t, AIF, kind='linear', bounds_error=False, fill_value=0)
-    tnew = np.linspace(0, t[-1], len(t))
-    AIF = f(tnew)
-    '''
+    tnew = np.linspace(0, t[-1], len(t)*10)
+    AIFnew = f(tnew)
 
-    # Perform now the convolution between the Arterial Input Function
-    # and the vascular impulse response
-    convolution = np.convolve(AIF, imp)
+    #Calculate CpKid - this is the convolution of the AIF with the VIRF g(t) which we assume is an exponential with time constant Tg, shifted by toff, with g(t<Delta)=0
+    g = (tnew >= Delta) * (1/Tg) * np.exp(-1 * (tnew - Delta) / Tg)
 
-    # Work out the Cd term
-    Cd = Ktrans * scipy.integrate.cumtrapz(convolution[0:len(t)], x=t, initial=0)
+    # Convolve impulse response with AIF, accounting for temporal resolution
+    CpKid = np.convolve(AIFnew, g)[0:len(tnew)] * tnew[1] 
 
-    # Adjust for the temporal resolution and add the second term.
-    # Return the overall array
-    # G = convolution[0:len(t)] * t[1] + vp * AIFnew
-    G = convolution[0:len(t)] * t[1] + vp * Cd
-    return G
+    vdCd = Ktrans * scipy.integrate.cumtrapz(CpKid, x=tnew, initial=0)
+
+
+    #Calculate Ct, this is the sum of vdCd and vpCpKid
+    Ct = vdCd + (vp * CpKid)
+
+    plt.plot(tnew, Ct)
+
+    return Ct
 
 # Create now function for initializng the parameters
 def InitializeParameters():
@@ -59,7 +62,6 @@ def InitializeParameters():
     Tg = MRT - delta
 
     # Initialize the volume fraction 
-    ve = Ktrans * Tg 
     return Ktrans, Tg, vp, delta
 
 def Initialize_InputParams():
@@ -67,31 +69,35 @@ def Initialize_InputParams():
     # mean value of 0.25 and standard deviation of 0.1 and 
     # convert the whole values from min^(-1) to s^(-1) and take the
     # absolute value
-    Ktrans = np.abs( np.random.normal(0.25, 0.1, 1000) / 60 )
-    
+    Ktrans = np.abs( np.random.normal(0.25, 0.1, 100) / 60 )
+
+    # Initialize the plasma volume in units of mm^3 
+    vp = np.random.uniform(0.2, 0.8, 100)
+
+    new_arr = np.array(np.meshgrid(Ktrans, vp)).T.reshape(-1, 2)
     # Take now the uniform random distribution for the 
     # delta variable from 1 to 3.5
-    delta = np.random.uniform(1, 3.5, 1000)
+    delta = np.random.uniform(1, 3.5, 10000)
 
     # Analyse now the distribution of the mean residence 
     # time MRT value - normal distribution of mean value 5.5
     # and standard deviation of 0.7
-    MRT = np.random.normal(5.5, 0.7, 1000)
+    MRT = np.random.normal(5.5, 0.7, 10000)
 
     # Now calculate the exponential decay time constant Tg
     Tg = MRT - delta 
 
-    # Initialize the plasma volume in units of mm^3 
-    vp = np.random.uniform(0.2, 0.8, 1000)
-
+    # NEW IMPLEMENTATION
+    # Declare a calibration factor for the K parameter ->
+    # switch from units of 1e-3 to units of 1
     # Return the following values: the Ktrans parameter, 
     # the exponential time decay constant Tg, the plasma 
     # volume, and the delta offset time
-    params = np.zeros((4, 1000))
-    params[0] = Ktrans
+    params = np.zeros((4, 10000))
+    params[0] = new_arr[:,0] * K_calibration
     params[1] = Tg
-    params[2] = vp 
-    params[3] = delta
+    params[2] = new_arr[:,1] 
+    params[3] = delta 
     return params.T
 
 def main():
@@ -100,17 +106,39 @@ def main():
     t = np.load('t.npy')
     AIF = np.load('AIF.npy')
 
+    # f = interpolate.interp1d(t, AIF, kind='linear', bounds_error=False, fill_value = 0)
+    tnew = np.linspace(0, t[-1], len(t)*10)
+    # AIFnew = f(tnew)
+    np.save('tnew.npy', tnew)
+
     # Initialize the array of (1000, 4) size with
     # the parameters to work out all the convoluted curves
+    params = InitializeParameters()
+    curve = ToftsModel(params, t, AIF)
+
+    print(curve.shape)
+    print(tnew.shape)
+
+    # plt.plot(AIF, color='b')
+    plt.plot(tnew, curve, color='red')
+    plt.show()
+
     params = Initialize_InputParams()
 
     # Now initialize an empty array of size (1000, 150) and
     # each line corresponds to a different TOFTS curve of 
     # parameters suggested by the previous params array
-    G = np.zeros((1000, 150))
-    for i in range(1000):
+    G = np.zeros((10000, 1500))
+    for i in range(10000):
         
+        # Calibrate back to the original scale for the K-value
+        params[i][0] = params[i][0] / K_calibration
+
+        # Now perform the Tofts Model curve
         G[i] = ToftsModel(params[i], t, AIF)
+
+        # Go back to the new scale
+        params[i][0] = params[i][0] * K_calibration
 
     # Save now the two arrays in .npy format
     np.save("data/synthetic/synthetic_curves.npy", G)
